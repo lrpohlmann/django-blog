f0a8b2ff5eeac1f44bc3bd564be2cc1dbf67b5b1
leonardo <leo.pohlmann@hotmail.com>
2023-01-28 15:07:12 -0300

    ok

--- blog/urls.py
+++ blog/urls.py
@@ -16,6 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+from posts.views import home_view
+
+urlpatterns = [path("admin/", admin.site.urls), path("", home_view)]
