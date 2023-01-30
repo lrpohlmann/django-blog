71782a832e2dd4f15c2b62a44b81333a8c435831
leonardo <leo.pohlmann@hotmail.com>
2023-01-29 22:47:21 -0300

    models

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from posts.models import Post, Assunto
+
 # Register your models here.
+@admin.register(Post)
+class PostsAdmin(admin.ModelAdmin):
+    fields = ("titulo", "autor", "corpo", "assuntos")
+
+
+@admin.register(Assunto)
+class AssuntoModel(admin.ModelAdmin):
+    pass
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,32 @@
+from typing import Iterable, Optional
+import uuid
+from django.template.defaultfilters import slugify
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=150, null=False, blank=False, unique=True)
+    data_criacao = models.DateField("data de criação", auto_now_add=True)
+    autor = models.ForeignKey(
+        User, on_delete=models.DO_NOTHING, null=False, blank=False
+    )
+    corpo = models.TextField(max_length=2000, null=False, blank=False)
+    assuntos = models.ManyToManyField("Assunto", null=False, blank=False)
+    slug = models.SlugField(null=True)
+    uuid = models.UUIDField(default=uuid.uuid4, null=False, blank=False)
+
+    def save(
+        self,
+        force_insert: bool = ...,
+        force_update: bool = ...,
+        using: Optional[str] = ...,
+        update_fields: Optional[Iterable[str]] = ...,
+    ) -> None:
+        if not self.slug:
+            self.slug = slugify(self.titulo)
+        return super().save(force_insert, force_update, using, update_fields)
+
+
+class Assunto(models.Model):
+    nome = models.CharField(max_length=15, null=False, blank=False, unique=True)
