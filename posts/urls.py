5b915e63b9b7b021db9a608a140b3b1c9b33b71b
leonardo <leo.pohlmann@hotmail.com>
2023-02-04 17:30:47 -0300

    post view

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
+from .views import post_view, home_view
+
 
 app_name = "posts"
 
-url_patterns = []
+urlpatterns = [
+    path("", home_view, name="home_view"),
+    path("post", post_view, name="post_view"),
+]
--- posts/views.py
+++ posts/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request):
     return render(request, "paginas/home.html")
+
+
+def post_view(request):
+    return render(request, "paginas/post.html")
