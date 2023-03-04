8b6f2eba2c3bf93d1de7a86402d68e3e9f08c033
leonardo <leo.pohlmann@hotmail.com>
2023-03-04 15:15:39 -0300

    paginação html

--- posts/tests.py
+++ posts/tests.py
@@ -16,12 +16,19 @@ class TestPostGridView(TestCase):
     def setUpTestData(cls):
         baker.make("Post", 20)
 
-    def test_post_grid_sem_pagina_indicada(self):
+    def test_sem_pagina_indicada(self):
         self.assertEqual(self.client.get("/post/grid").status_code, 200)
 
-    def test_post_grid_com_pagina_indicada(self):
+    def test_com_pagina_indicada(self):
         for n in range(0, 10):
             with self.subTest(msg=f"Testando com página número {n}"):
                 self.assertEqual(
                     self.client.get("/post/grid", data={"pagina": n}).status_code, 200
                 )
+
+    def test_paginador_apareceu(self):
+        resposta = self.client.get("/post/grid", data={"pagina": 2})
+        html = str(resposta.content).replace(" ", "").replace("\\n", "")
+        self.assertIn("1</button>", html)
+        self.assertIn("3</button>", html)
+        self.assertIn("2</div>", html)
